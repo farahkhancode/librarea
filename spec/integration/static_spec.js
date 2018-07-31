b0b0a1d6c8e0844e5d2fc6e43ad7cdd65efacbae
farahmed@gmail.com <farahmed@gmail.com>
2018-07-31 11:11:39 -0400

    WIP deploy to production

---
+++ spec/integration/static_spec.js
@@ -0,0 +1,23 @@
+const request = require("request");
+const server = require("../../src/server");
+const base = "http://localhost:3000/";
+
+describe("routes : static", () => {
+
+//#1
+  describe("GET /", () => {
+
+//#2
+    it("should return status code 200", (done) => {
+
+//#3
+      request.get(base, (err, res, body) => {
+        expect(res.statusCode).toBe(200);
+
+//#4
+        done();
+      });
+    });
+
+  });
+});
